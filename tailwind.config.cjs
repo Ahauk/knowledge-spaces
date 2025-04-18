d27195ab63e43ac0712bf053ff090ced79d48b0a
Ahauk <victor.manuel.suarezp@gmail.com>
2025-04-18 02:13:14 -0600

    add custom classes

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,17 @@
 module.exports = {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      boxShadow: {
+        postit: '8px 8px 8px 2px rgba(0, 0, 0, 0.1)',
+      },
+      backgroundColor: {
+        cardBackground: '#f2fefe',
+      },
+      borderRadius: {
+        cardRounded: '32px',
+      },
+    },
   },
   plugins: [],
 };
